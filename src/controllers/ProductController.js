a3d6a95b43440474b471f9078cafb1adea2f3813
Jhony Rodrigues <rsjhony@hotmail.com>
2020-12-02 23:13:27 -0300

    Routes, Database

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Product = mongoose.model('Product');
+
+module.exports = {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+    const products = await Product.paginate({}, { page, limit: 10 });
+
+    return response.json(products)
+  },
+
+  async show(request, response) {
+    const product = await Product.findById(request.params.id);
+
+    return response.json(product);
+  },
+
+  async create(request, response) {
+    const product = await Product.create(request.body);
+
+    return response.json(product);
+  },
+
+  async update(request, response) {
+    const product = await Product.
+    findByIdAndUpdate(request.params.id, request.body, { new: true }
+    );
+
+    return response.json(product)
+  },
+
+  async destroy(request, response) {
+    await Product.findByIdAndDelete(request.params.id);
+
+    return response.status(204).send();
+  },
+};
